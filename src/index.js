be41f32337faa3cfe48771b9b33d5598f2b0760e
louispaulromero <louispaulromero@gmail.com>
2025-08-12 01:07:02 -0700

    clean branch again

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles.css"; // Your CSS file
-import '/bootstrap/js/bootstrap.bundle.js';
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
